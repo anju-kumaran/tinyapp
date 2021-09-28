f4af0035f5691153b6ad99979bf864ba7421d6c3
anju-kumaran <anju.naduth@gmail.com>
2021-09-28 09:49:30 -0400

    Redirecting after form submission

--- express_server.js
+++ express_server.js
@@ -2,12 +2,27 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+function generateRandomString() {
+  let randomStr = '';
+  const alphaNum = 'ABCDEFGHIJKLMNOPQRSTUVWXYXabcdefghijklmnopqrstuvwxy1234567890';
+  for (let i = 0; i < 6; i++) {
+    let randomNum = Math.floor(Math.random() * 56 + 1);
+    randomStr += alphaNum[randomNum]
+  }
+  //console.log("here is the random url: ", randomStr);
+  return randomStr;
+}
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -22,10 +37,30 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.b2xVn2 };
+  const longURL= urlDatabase[req.params.shortURL];
+  const templateVars = { shortURL: req.params.shortURL, longURL: longURL};
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+// app.get("/urls/:shortURL", (req, res) => {
+//   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.b2xVn2 };
+//   res.render("urls_show", templateVars);
+// });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
